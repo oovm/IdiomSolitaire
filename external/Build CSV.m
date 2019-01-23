cd502f49ed7901f2de69594cdb7e99b3a2326ae2
Aster <galaster@foxmail.com>
2019-01-23 21:22:18 +0800

    数据::optimize data export

--- external/Build CSV.m
+++ external/Build CSV.m
@@ -1,6 +1,14 @@
 (* ::Package:: *)
 
+(* ::Section:: *)
+(*Setting*)
+
+
 SetDirectory@NotebookDirectory[];
+
+
+(* ::Section:: *)
+(*Data*)
 
 
 data = GeneralUtilities`Scope[
@@ -12,17 +20,36 @@ data = GeneralUtilities`Scope[
 	tmp = Import["Source_1.mx", "CSV"];
 	Echo[Length@tmp, "Records:"];
 	tmp[[All, {2, 3, 4}]]
-]
+];
 
 
 data = MapAt[StringRiffle@*StringSplit, data, {All, 2}]
 
 
+(* ::Section:: *)
+(*Export*)
+
+
+(* ::Subsection:: *)
+(*Export*)
+
+
 Export[
-	SortBy[data, Rest],
-	"Database.csv", "CSV",
-	"TableHeadings" -> {"Idiom", "Pinyin", "Explanation"}
+	"database.csv",
+	SortBy[data, Rest], "CSV",
+	"TableHeadings" -> {"Idiom", "Pinyin", "Explanation"},
+	CharacterEncoding -> "UTF8"
 ]
 
 
+(* ::Subsection:: *)
+(*Export Accelerate*)
 
+
+(* ::Input:: *)
+(*string = ExportString[*)
+(*	SortBy[data, Rest], "CSV",*)
+(*	"TableHeadings" -> {"Idiom", "Pinyin", "Explanation"},*)
+(*	CharacterEncoding -> "UTF8"*)
+(*];*)
+(*Export["database.csv", string, "Table", CharacterEncoding -> "UTF8"]*)
