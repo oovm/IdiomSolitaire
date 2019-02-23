c45883c4f16ff58e51fa458550571450bc0eac35
Aster <galaster@foxmail.com>
2019-02-24 00:45:29 +0800

    数据::add killer data

--- typescript/source/data/index.ts
+++ typescript/source/data/index.ts
@@ -1,3 +1,18 @@
-import * as SelfReference from "./SelfReference";
+import * as S from "./SelfReference";
+import * as K from "./Killer"
 
-console.log(SelfReference.Character[1])
+export const SelfReference = {
+    Character: S.SelfReferenceCharacter.map(x => x.Idiom),
+    Pinyin: S.SelfReferencePinyin.map(x => x.Idiom),
+    Letter: S.SelfReferenceLetter.map(x => x.Idiom),
+}
+
+export const Killer = {
+    Character: K.KillCharacter,
+    Pinyin: K.KillPinyin.map(x => x.Idiom),
+    Letter: K.KillLetter.map(x => x.Idiom),
+}
+
+console.log(SelfReference)
+
+console.log(Killer)
