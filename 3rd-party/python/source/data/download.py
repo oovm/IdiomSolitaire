54af3450cd51afd150d82702a201001e41e2a037
Aster <galaster@foxmail.com>
2019-02-25 17:38:58 +0800

    重构::put python into 3rd-party

---
+++ 3rd-party/python/source/data/download.py
@@ -0,0 +1,17 @@
+class Config:
+    idioms = {
+        "name": "",
+        "url": "",
+        "md5": ""
+    }
+
+
+
+
+print("Database not found")
+
+
+# if md5 not equal
+print("MD5 not match, updating to new version now")
+
+
